ffb6fbca0523819785a85d961962321106d399b8
Effet <nesuadark@gmail.com>
2015-02-18 14:38:41 +0800

    just brute force

--- codeforces/515B.rb
+++ codeforces/515B.rb
@@ -1,31 +1,20 @@
 #!/usr/bin/env ruby
 
 n, m = gets.split.map(&:to_i)
-xs = gets.split.map(&:to_i)
-ys = gets.split.map(&:to_i)
+_, *xs = gets.split.map(&:to_i)
+_, *ys = gets.split.map(&:to_i)
 
 x_happy = Array::new(n, false)
 y_happy = Array::new(m, false)
 
-xs[1, xs[0]].each do |x|
-  x_happy[x] = true
-end
+xs.each { |i| x_happy[i] = true }
+ys.each { |i| y_happy[i] = true }
 
-ys[1, ys[0]].each do |y|
-  y_happy[y] = true
-end
-
-g = n.gcd(m)
-
-[x_happy.length, y_happy.length].min.times {
-  x_happy.length.times do |i|
-    y_happy.length.times do |j|
-      if (i - j).abs % g == 0 && (x_happy[i] || y_happy[j])
-        x_happy[i] = y_happy[j] = true
-      end
-    end
+(n*m*2).times do |i|
+  if x_happy[i%n] || y_happy[i%m]
+    x_happy[i%n] = y_happy[i%m] = true
   end
-}
+end
 
 if x_happy.all? && y_happy.all?
   puts "Yes"
